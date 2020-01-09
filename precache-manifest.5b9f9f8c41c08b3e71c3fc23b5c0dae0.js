40a4a012dd649ba4f5fdef1a154c3a35c3bca861
comradem <pperedot@gmail.com>
2020-01-09 13:29:36 +0200

    Updates

---
+++ precache-manifest.5b9f9f8c41c08b3e71c3fc23b5c0dae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66b9b1d028cbb8b736efc0a9740d752",
+    "url": "/feesdale/index.html"
+  },
+  {
+    "revision": "b80283c15c2781283eff",
+    "url": "/feesdale/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8897e2e502c35c3c7c5a",
+    "url": "/feesdale/static/js/2.ce037d12.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/feesdale/static/js/2.ce037d12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b80283c15c2781283eff",
+    "url": "/feesdale/static/js/main.ce1a113a.chunk.js"
+  },
+  {
+    "revision": "ec32d2d0e9d83059362d",
+    "url": "/feesdale/static/js/runtime-main.de5279ab.js"
+  }
+]);
