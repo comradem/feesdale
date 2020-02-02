92be4bce6f8531c6df21a50af758dd9bed217c48
comradem <pperedot@gmail.com>
2020-02-02 13:17:18 +0200

    filtering works

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,6 +6,27 @@ import '../styles/main.css'
 
 class MainPage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchField : ''
+        }
+    }
+
+
+    handleSearchInput = (event) => {
+        // this.props.searchText = event.target.value;
+        this.setState({
+            searchField : event.target.value
+        })
+    };
+
+
+    handleSearch = () => {
+        let filtered = this.props.searchData.filter(item => item.searchKeywords.join(" ").includes(this.state.searchField));
+        this.props.history.push('/store',filtered);
+    };
+
 
     render() {
         return (
@@ -13,7 +34,7 @@ class MainPage extends Component {
                 <div className='main-page-logo'>MAX SUCCESS</div>
                 <Form>
                     <Form.Group controlId="formSearch">
-                        <Form.Control type="text" placeholder="Search ..."/>
+                        <Form.Control type="text" placeholder="Search ..." onChange={this.handleSearchInput}/>
                     </Form.Group>
                     <div className='search-bar-buttons'>
                         <Button variant="secondary" type="button" className='p-main-search-btn'
