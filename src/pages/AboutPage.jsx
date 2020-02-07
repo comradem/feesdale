999735023bf1e30cf1f67b9599b83e5092a3de59
comradem <pperedot@gmail.com>
2020-02-07 18:50:09 +0200

    about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
+import '../styles/about.css'
 
 class AboutPage extends Component {
     render() {
         return (
             <div className='container'>
                 <div>
-                    this page should contain store info
+                    MAX Success
+                    <br/>
+                    Get ready for Spring 2020!
+                    <br/>
+                    New Collection already here
+                    <br/>
+                    Come and check out
+                    <br/>
+                    Sales on winter collection
+                    <div>
+                        Address
+                    </div>
                 </div>
             </div>
         );
     }
 }
-
 export default AboutPage;
